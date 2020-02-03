b17e2be83f0fd40ac21d8d21e14cafaea63f75ec
Warren Harper II <warrenharper2@att.net>
2020-02-03 18:36:58 +0000

    Automatically backed up by Learn

--- lib/support/db_registry.rb
+++ lib/support/db_registry.rb
@@ -1,6 +1,7 @@
 require_relative 'connection_adapter'
 
-DBRegistry ||= OpenStruct.new(test: ConnectionAdapter.new("db/#{DBNAME}-test.db"),
-  development: ConnectionAdapter.new("db/#{DBNAME}-development.db"),
-  production: ConnectionAdapter.new("db/#{DBNAME}-production.db")
-  )
+#DBRegistry ||= OpenStruct.new(test: ConnectionAdapter.new("db/#{DBNAME}-test.db"),
+  #development: ConnectionAdapter.new("db/#{DBNAME}-development.db"),
+
+  #3production: ConnectionAdapter.new("db/#{DBNAME}-production.db")
+  #)
